2bbe576370e1c1cad619625d0e77b4683dae75a2
EvanHuang <evan.huang@oocl.com>
2019-02-18 19:51:40 +0800

    apply config to order
    其中application.yml必须改名为boostratp.yml,  意思是先加载boostrap里面 信息，然后再读取git上config 信息，
    如果不改名字话， 将会报数据库相关的错误， 因为spring不知道先加载哪一部分

--- order/order_server/src/main/java/com/jluzh/order/controller/EnvController.java
+++ order/order_server/src/main/java/com/jluzh/order/controller/EnvController.java
@@ -7,16 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//@RestController
-//@RequestMapping("/env")
-//@RefreshScope
-//public class EnvController {
-//
-//    @Value("${env}")
-//    private String env;
-//
-//    @GetMapping("/print")
-//    public String print() {
-//        return env;
-//    }
-//}
+@RestController
+@RequestMapping("/env")
+@RefreshScope
+public class EnvController {
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/print")
+    public String print() {
+        return env;
+    }
+}
