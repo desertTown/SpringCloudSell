f72044dc262ceaf1d668053500f005fb85d1c635
EvanHuang <evan.huang@oocl.com>
2019-02-28 21:44:36 +0800

    继承 HystrixDashboard 功能
    actuator/hystrix.stream
    localhost:8864/hystrix  - 》 输入三个参数 http://localhost:8764/actuator/hystrix.stream， 1000， Order
    
    请求Order服务接口， 好看dashboad变化

--- order/order_server/src/main/java/com/jluzh/order/OrderApplication.java
+++ order/order_server/src/main/java/com/jluzh/order/OrderApplication.java
@@ -1,12 +1,9 @@
 package com.jluzh.order;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -18,6 +15,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringCloudApplication    //这个注解充当 @SpringBootApplication，@EnableDiscoveryClient@EnableCircuitBreaker
 //主要是为了能扫描到这个类com.jluzh.product.client.ProductClient, 扩大扫描范围
 @ComponentScan(basePackages = "com.jluzh")
+@EnableHystrixDashboard
+@EnableHystrix
 public class OrderApplication {
 
     public static void main(String[] args) {
